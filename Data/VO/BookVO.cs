d6109cbb2f146fcec104116a7bdcd83fcd3891d3
Malu Fell <malufell@hotmail.com>
2024-01-20 19:08:42 -0300

    chore: add serialization example

--- Data/VO/BookVO.cs
+++ Data/VO/BookVO.cs
@@ -15,3 +15,26 @@ public class BookVO
 
   public string Title { get; set; }
 }
+
+/*
+exemplo de serialização dos dados
+
+using System.Text.Json.Serialization;
+public class BookVO
+{
+  [JsonPropertyName("code")]
+  public long Id { get; set; }
+
+  [JsonPropertyName("name")]
+  public string Author { get; set; }
+
+  [JsonPropertyName("date")]
+  public DateTime LaunchDate { get; set; }
+
+  [JsonIgnore] > não vai retornar esse campo
+  public decimal Price { get; set; }
+
+  [JsonPropertyName("book_name")]
+  public string Title { get; set; }
+}
+*/
